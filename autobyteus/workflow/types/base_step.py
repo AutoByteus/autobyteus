1144263a7597bb5f994e8a124cbde4f940b8aaee
Ryan Zheng <nogrethumphrey@gmail.com>
2023-10-04 07:38:47 +0200

    added module specification documentation

--- autobyteus/workflow/types/base_step.py
+++ autobyteus/workflow/types/base_step.py
@@ -1,42 +1,38 @@
 """
-autobyteus/workflow/types/base_step.py
+autobyteus/workflow_types/types/base_step.py
 
 This module contains the BaseStep class, which serves as an abstract base class for all steps in the automated coding
-workflow. Each step in the workflow should inherit from this class, implement the required methods, and provide versioning
-capabilities through the PromptVersioningMixin. The BaseStep class provides a foundation for creating custom steps with 
-unique IDs, names, dynamic prompt construction, and version management.
+workflow. Each step in the workflow should inherit from this class and implement the required methods. The BaseStep
+class provides a foundation for creating custom steps with unique IDs, names, and prompt construction.
 
 BaseStep class features:
 - Unique ID generation for each step instance.
 - A class attribute for the step name.
 - Abstract methods for constructing prompts and processing responses that need to be implemented in derived classes.
 - A method called execute for triggering the step's execution, which needs to be implemented in derived classes.
-- Versioning capabilities for managing different prompt versions.
 
 To create a new step, inherit from the BaseStep class, set the step name using the set_step_name class method,
-implement the required abstract methods, the execute method, and provide a default_prompt for versioning.
+and implement the required abstract methods and the execute method.
 """
 
 from abc import ABC, abstractmethod
 from autobyteus.prompt.prompt_template import PromptTemplate
-from autobyteus.prompt.prompt_versioning_mixin import PromptVersioningMixin
 from autobyteus.workflow.types.base_workflow import BaseWorkflow
 from autobyteus.workflow.utils.unique_id_generator import UniqueIDGenerator
 
 
-class BaseStep(ABC, PromptVersioningMixin):
+class BaseStep(ABC):
     """
     BaseStep is the abstract base class for all steps in the automated coding workflow.
-    Each step should inherit from this class, implement the required methods, and offer versioning capabilities.
+    Each step should inherit from this class and implement the required methods.
     """
 
     name = None
+    prompt_template: PromptTemplate = None  # Instance of PromptTemplate
 
     def __init__(self, workflow: BaseWorkflow):
-        super().__init__()
         self.id = UniqueIDGenerator.generate_id()
         self.workflow = workflow
-        self.default_prompt = self.construct_prompt()  # Assuming the default prompt is constructed using this method
 
     def to_dict(self) -> dict:
         """
@@ -48,22 +44,18 @@ class BaseStep(ABC, PromptVersioningMixin):
         return {
             "id": self.id,
             "name": self.name,
-            "prompt_template": self.prompt_template.to_dict() if self.prompt_template else None,
-            "current_version": self.load_latest_version()  # Load the current version of the prompt
+            "prompt_template": self.prompt_template.to_dict() if self.prompt_template else None
         }
 
+    @abstractmethod
     def construct_prompt(self) -> str:
         """
-        Construct the prompt for this step using the versioning system. If no version is available in the database,
-        it falls back to the default prompt provided by the step.
+        Construct the prompt for this step.
 
         Returns:
             str: The constructed prompt for this step.
         """
-        current_prompt_version = self.load_latest_version()
-        if not current_prompt_version:
-            return self.default_prompt
-        return current_prompt_version.prompt_content
+        pass
 
     @abstractmethod
     def process_response(self, response: str) -> None:
