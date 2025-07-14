aae1c63e48376b4448a59579676c5b57df8405e7
Ryan Zheng <nogrethumphrey@gmail.com>
2025-07-14 11:52:09 +0200

    change the IMAGE_CONTEXT to IMAGE, and increase the library version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ common_dependencies = [
 
 setup(
     name="autobyteus",
-    version="1.1.0",
+    version="1.1.1",
     packages=find_packages(exclude=["tests", "tests.*"]),
     include_package_data=True,
     install_requires=[
