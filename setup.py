41a0c4d098f7914aa7afe1f5531aa2cd0726ce45
Ryan Zheng <nogrethumphrey@gmail.com>
2025-10-11 05:18:55 +0200

    chore: Bump version to 1.2.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ common_dependencies = [
 
 setup(
     name="autobyteus",
-    version="1.1.9",
+    version="1.2.0",
     packages=find_packages(exclude=["tests", "tests.*"]),
     include_package_data=True,
     install_requires=[
